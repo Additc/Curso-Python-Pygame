5e82c2eefefa514852e7ff2c7b4f699709633605
AddiSC2 <chavezaddi12@gmail.com>
2025-04-23 16:04:11 -0600

    Snake Game

--- Snake_game/version_0_6/Apple.py
+++ Snake_game/version_0_6/Apple.py
@@ -1,12 +1,14 @@
 import pygame
 from pygame.sprite import Sprite
 from Configuration import Configurations
+from random import randint
+
 
 class Apple(Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image=pygame.Surface(Configurations.get_apple_block_size())
+        self.image=pygame.Surface((Configurations.get_apple_block_size(),Configurations.get_snake_block_size()))
         self.image.fill(Configurations.get_apple_color())
 
         self.rect=self.image.get_rect()
@@ -17,3 +19,14 @@ class Apple(Sprite):
         :param screen: Pantalla en donde se dibuja.
         """
         screen.blit(self.image,self.rect)
+
+    def random_position(self)->None:
+        """
+        Se utiliza para inicializar una ubicación aleatoria de una manzana.
+        """
+        screen_width=Configurations.get_screen_size()[0]
+        screen_height=Configurations.get_screen_size()[1]
+        apple_block_size=Configurations.get_apple_block_size()
+
+        self.rect.x= apple_block_size*randint(0,(screen_width//apple_block_size-1))
+        self.rect.y= apple_block_size*randint(0,(screen_height//apple_block_size-1))
