c4f034bd0e5fd6949f6df393ff91b10e0c6bb905
AddiSC2 <chavezaddi12@gmail.com>
2025-04-24 15:58:16 -0600

    Snake Game

---
+++ Snake_game/version_0_7/Snake06_game.py
@@ -0,0 +1,70 @@
+"""
+Nombre: Addi Toro Chávez
+fecha: 22 de abril del 2025
+versión: 0.6
+-Se aagrega bloques al cuerpo de la serpiente.
+"""
+
+
+#Se importan lo módulos para el videojuego
+import pygame
+from Configuration import Configurations
+from Game_funtionalities import game_events,screen_refresh,snake_movement
+from Snake import  SnakeBlock
+from pygame.sprite import Group
+from Apple import Apple
+
+from Snake_game.version_0_6.Apple import Apple
+
+
+def run_game()->None:
+    """
+    Función principal del videojuego
+    :return:
+    """
+    #Se inicializa el módulo de pygame
+    pygame.init()
+
+    #Se configura el reloj del juego
+    clock=pygame.time.Clock()
+
+    #Se inicializa la pantalla
+    screen=pygame.display.set_mode(Configurations.get_screen_size())
+
+    #Se configura el título del juego
+    pygame.display.set_caption(Configurations.get_game_title())  #Mostrar título
+
+    #Se crea el bloque inicial de la serpiente (cabeza)
+    snake_head=SnakeBlock(is_head=True)
+    snake_head.snake_head_init()
+
+    #Se crea un grupo para almacenar el cuerpo de la serpiente.
+    snake_body=Group()
+    snake_body.add(snake_head)
+
+    #Se crea el bloque inicial de la manzana
+    apple=Apple()
+    apple.random_position()
+
+    #Se crea un grupo con las manzana
+    apples = Group()
+    apples.add(apple)
+
+    #Ciclo principal del videojuego
+    game_over=False
+
+    while not game_over:
+        #Se verifican los eventos (teclado,ratón) del juego.
+        game_over=game_events(snake_body,apples)
+
+        #Se administra el movimiento de la serpiente
+        snake_movement(snake_body)
+
+        #Se dibujan los elementos gráficos en la pantalla
+        screen_refresh(screen, clock,snake_body,apples)
+
+    #Se cierran los recursos de pygame
+    pygame.quit()
+
+if __name__ == '__main__':
+    run_game()
---
+++ Snake_game/version_0_8/Game_funtionalities.py
@@ -0,0 +1,148 @@
+import pygame
+from Configuration import Configurations
+from Snake import SnakeBlock
+from Apple import Apple
+
+def game_events(snake_body:pygame.sprite.Group,
+                apples:pygame.sprite.Group)->bool:
+    """
+    Función que administra los evento de juego
+    :return: la bandera del fin del juego
+    """
+    game_over=False
+
+    # Se verifican los eventos (teclado,ratón) del juego.
+    for event in pygame.event.get():
+        # Un clic en cerrar el juego
+        if event.type == pygame.QUIT:
+            game_over = True
+
+        #Se verifica que se precione una tecla
+        if event.type == pygame.KEYDOWN:
+            #Verificar que tecla presiona
+            if event.key == pygame.K_RIGHT: #Indica que se mueve hacia la derecha
+                SnakeBlock.set_is_moving_right(True)
+                SnakeBlock.set_is_moving_left(False)
+                SnakeBlock.set_is_moving_up(False)
+                SnakeBlock.set_is_moving_down(False)
+
+            if event.key == pygame.K_LEFT: #Indica que se mueve hacia la izquierda
+                SnakeBlock.set_is_moving_right(False)
+                SnakeBlock.set_is_moving_left(True)
+                SnakeBlock.set_is_moving_up(False)
+                SnakeBlock.set_is_moving_down(False)
+
+            if event.key == pygame.K_UP: #Indica que se mueve hacia  arriba
+                SnakeBlock.set_is_moving_right(False)
+                SnakeBlock.set_is_moving_left(False)
+                SnakeBlock.set_is_moving_up(True)
+                SnakeBlock.set_is_moving_down(False)
+
+            if event.key == pygame.K_DOWN: #Indica que se mueve hacia abajo
+                SnakeBlock.set_is_moving_right(False)
+                SnakeBlock.set_is_moving_left(False)
+                SnakeBlock.set_is_moving_up(False)
+                SnakeBlock.set_is_moving_down(True)
+
+            if event.key == pygame.K_SPACE:
+                new_snake_block=SnakeBlock()
+                snake_body.add(new_snake_block)
+
+                new_apple=Apple()
+                new_apple.random_position(snake_body)
+                #print(Apple.get_no_apples())
+
+                #apples.remove(apples.sprites()[0])
+                apples.empty()
+                apples.add(new_apple)
+
+    #Se regresa la bandera
+    return game_over
+
+def snake_movement(snake_body:pygame.sprite.Group)->None:
+    """
+    Función que gestona el movimiento del cuerpo de la
+    serpiente.
+    :param sanke_body: Grupo von el cuerpo de la serpiente.
+    """
+    body_size=len(snake_body.sprites())-1
+
+    for i in range(body_size,0,-1):
+        snake_body.sprites()[i].rect.x=snake_body.sprites()[i-1].rect.x
+        snake_body.sprites()[i].rect.y=snake_body.sprites()[i-1].rect.y
+
+    head=snake_body.sprites()[0]
+
+    if SnakeBlock.get_is_moving_right():
+        head.rect.x += Configurations.get_snake_block_size()
+
+    elif SnakeBlock.get_is_moving_left():
+        head.rect.x -= Configurations.get_snake_block_size()
+
+    elif SnakeBlock.get_is_moving_up():
+        head.rect.y -= Configurations.get_snake_block_size()
+
+    elif SnakeBlock.get_is_moving_down():
+        head.rect.y += Configurations.get_snake_block_size()
+
+def check_collision(screen: pygame.surface.Surface,
+                    snake_body:pygame.sprite.Group,
+                    apples:pygame.sprite.Group)->bool:
+    """
+    Función que revisa las colisiones del juego:
+    *Cabeza de la serpiente con el cuerpo.
+    *Cabeza de la serpiente con el borde de la pantalla.
+    *Cabeza de la serpiente con la manzana.
+    :param screen: Pantalla
+    :param snake_body: Cuerpo de la serpiente.
+    :param apples: Grupo de las manzanas.
+    :return: Las banderas de  fin de juego.
+    """
+
+    #Se declara la bandera de fin de juego
+    game_over=False
+
+    #Se obtiene la cabeza de la serpiente
+    head=snake_body.sprites()[0]
+
+    #Se revisa la condición de la cabeza de la serpiente con la pantalla
+    screen_rect=screen.get_rect()
+
+    if head.rect.right>screen_rect.right:
+        game_over=True
+    if head.rect.left<screen_rect.left:
+        game_over=True
+    if head.rect.top<screen_rect.top:
+        game_over=True
+    if head.rect.bottom>screen_rect.bottom:
+        game_over=True
+
+    return game_over
+
+
+
+def screen_refresh(screen: pygame.surface.Surface,
+                   clock:pygame.time.Clock,
+                   snake_body:pygame.sprite.Group,
+                   apples:pygame.sprite.Group)->None:
+    """
+    Función que administra los elementos visuales del juego.
+    :return:
+    """
+    #Fondo de la pantalla en formato RGB
+    screen.fill(Configurations.get_background())
+
+
+    #Se dibuja el cuerpo de la serpiente
+    for snake_block in reversed (snake_body.sprites()):
+        snake_block.blit(screen)
+
+    #Se dibuja la manzana
+    apples.draw(screen)
+
+
+    #Se actuliza la pantalla
+    pygame.display.flip()
+
+    #Se controla velocidad de pantalla de FPS del juego
+    clock.tick(Configurations.get_fps())
