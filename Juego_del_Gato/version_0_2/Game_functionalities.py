a91b5c6b65b2f6e21cbc9605ff880019b4737990
Addilap <chavezaddi12@gmail.com>
2025-05-13 16:50:57 -0600

    Juego del gato

---
+++ Juego_del_Gato/version_0_2/Game_functionalities.py
@@ -0,0 +1,28 @@
+import pygame
+from Configurations import Configurations
+
+def game_events()->bool:
+    """
+    Función que administra los eventos de juego.
+    :return: la bandera del fin del juego.
+    """
+    game_over=False
+
+    # Se verifican los eventos (teclado, ratón) del juego.
+    for event in pygame.event.get():
+        # Un clic en cerrar el juego
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    #Se regresa la bandera
+    return game_over
+
+def screen_refresh(screen: pygame.surface.Surface)->None:
+    """
+    Función que administra los elementos visuales del juego.
+    """
+    #Fondo de la pantalla en formato RGB
+    screen.fill(Configurations.get_background())
+
+    #Se actualiza la pantalla
+    pygame.display.flip()
